6553ca93b0f63c3c42159f0be589bcb9583232a3
GregDungca <greg.dungca@gmail.com>
2015-10-27 18:26:54 -0700

    Added utils and refactored to put URL checking in basic server

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,14 +5,6 @@ var requestHandler = function(request, response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var paths = [
-    '/classes/chatterbox',
-    '/classes/room1',
-    '/classes/room',
-    '/log',
-    '/classes/messages'
-  ];  
-  
   var getHandler = function () {
     var messages = fs.createReadStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
     messages.setEncoding('utf8');
@@ -62,30 +54,13 @@ var requestHandler = function(request, response) {
     'OPTIONS' : optionsHandler
   }
 
-
-  if ( paths.indexOf(url.parse(request.url).pathname) !== -1 ) {
-    var action = request.method;
-    if ( methodHandler[action] ) {
-      methodHandler[action]();
-    } else {
-      utils.sendResponse(response, 'NOT FOUND', 404);
-    }
+  var action = request.method;
+  if ( methodHandler[action] ) {
+    methodHandler[action]();
   } else {
     utils.sendResponse(response, 'NOT FOUND', 404);
   }
 
 };
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-
-
 
 exports.requestHandler = requestHandler;
--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,13 @@
+// These headers will allow Cross-Origin Resource Sharing (CORS).
+// This code allows this server to talk to websites that
+// are on different domains, for instance, your chat client.
+//
+// Your chat client is running from a url like file://your/chat/client/index.html,
+// which is considered a different domain.
+//
+// Another way to get around this restriction is to serve you chat
+// client from this domain by setting up static file serving.
+
 var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
