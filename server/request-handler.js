648c5816beefa401aeb81cfe9eb1b668bb6d4cbf
GregDungca <greg.dungca@gmail.com>
2015-10-27 18:04:37 -0700

    Refactored method handling with object literals

--- server/request-handler.js
+++ server/request-handler.js
@@ -25,62 +25,70 @@ var requestHandler = function(request, response) {
     response.writeHead(statusCode, headers);
     response.end(data);
   }
+  
+
+  
+
+  var getHandler = function () {
+    var messages = fs.createReadStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
+    messages.setEncoding('utf8');
+    var messageData = '';
+    messages.on('data', function(chunk) {
+      messageData += chunk;
+    });
+
+    messages.on('end', function() {
+      sendResponse(response, messageData, 200);
+    });
+  }
+
+  var postHandler = function () {
+    console.log('received a POST message');
+    var receivedMessage = '';
+    request.setEncoding('utf8');
+
+    request.on('data', function(chunk) {
+      receivedMessage += chunk;
+    });
+    var readMessages = fs.createReadStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
+    readMessages.setEncoding('utf8');
+    var allData = '';
+    readMessages.on('data', function(chunk) {
+      allData += chunk;
+    });
+    
+
+    readMessages.on('end', function() {
+      var writeStream = fs.createWriteStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
+      writeStream.end(allData.slice(0, allData.length-2) + ',' + receivedMessage + "]}");
+      writeStream.on('finish', function () {
+        sendResponse(response, '{"objectID":0}', 201)
+      });
+    });
+  }
+
+  var optionsHandler = function () {
+    sendResponse(response, '', 200);
+    console.log('test');
+  }
+
+  var methodHandler = {
+    'GET' : getHandler,
+    'POST' : postHandler,
+    'OPTIONS' : optionsHandler
+  }
 
 
   if ( paths.indexOf(url.parse(request.url).pathname) !== -1 ) {
-
-    if ( request.method === 'POST') {
-      console.log('received a POST message');
-      var receivedMessage = '';
-      request.setEncoding('utf8');
-
-      request.on('data', function(chunk) {
-        receivedMessage += chunk;
-      });
-      var readMessages = fs.createReadStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
-      readMessages.setEncoding('utf8');
-      var allData = '';
-      readMessages.on('data', function(chunk) {
-        allData += chunk;
-      });
-      
-
-      readMessages.on('end', function() {
-        var writeStream = fs.createWriteStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
-        writeStream.end(allData.slice(0, allData.length-2) + ',' + receivedMessage + "]}");
-        writeStream.on('finish', function () {
-          sendResponse(response, '{"objectID":0}', 201)
-        });
-      });
-
+    var action = request.method;
+    if ( methodHandler[action] ) {
+      methodHandler[action]();
+    } else {
+      sendResponse(response, 'NOT FOUND', 404);
     }
-
-    else if ( request.method === 'GET' ) {
-      var messages = fs.createReadStream('/Users/student/2015-10-chatterbox-server/server/messages.json');
-
-      messages.setEncoding('utf8');
-
-      var messageData = '';
-      messages.on('data', function(chunk) {
-        messageData += chunk;
-      });
-
-      messages.on('end', function() {
-        sendResponse(response, messageData, 200);
-      });
-    }
-
-    else if ( request.method === 'OPTIONS' ) {
-      sendResponse(response, '', 200);
-    }
-    
+  } else {
+    sendResponse(response, 'NOT FOUND', 404);
   }
-
-  else {
-    sendResponse(response, '', 404);
-  }
-
-
 
 };
 
